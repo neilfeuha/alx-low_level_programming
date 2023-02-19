3f3c6cc341b54650e969422f421ebf4498fc8391
neilfeuha <feuhaneil@gmail.com>
2023-02-19 02:59:29 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -9,14 +9,14 @@
  *
  * description - program ends in in a new line character
  *
- * return 0
+ * return - always a 0
  */
 
 int main(void)
 
 {
 
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 
 }
