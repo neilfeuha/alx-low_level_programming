56c8c3d0ef57a7ba879351f6fac1a89c054bc9f5
neilfeuha <feuhaneil@gmail.com>
2023-02-19 03:04:18 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - program uses puts function and not printf
+ * main - " program uses puts function and not printf
  *
  * description - program outputs 'programming is like building a
  *
  * multilingual puzzle
  *
- * description - program ends in in a new line character
+ * description - program ends in in a new line character."
  *
- * return - always a 0
+ * return 0.
  */
 
 int main(void)
